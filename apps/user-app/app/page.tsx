c64dc21a391d07d08e05e24d33c87609f03024dd
Aryan Barfa <barfatech05@gmail.com>
2025-07-11 11:39:30 +0530

    v2

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 import HomePage from "../components/LandingPage";
+import { div } from "framer-motion/client";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
@@ -10,7 +11,9 @@ export default async function Page() {
   }
   return(
     <>
-    <HomePage/></>
+    <HomePage/> 
+    <div></div></>
+     
   )
 }
 
